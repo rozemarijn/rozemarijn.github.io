0f29118ac3b962a1b68c0c3e1cb91f8e8b2e2fed
rozemarijn <rozemarijnkolenbrander@gmail.com>
2014-04-08 11:56:54 +0200

    -
    -

--- java.js
+++
@@ -1,6 +0,0 @@
- window.onload = function () {
-    'use strict';
-    
- document.getElementById("procent").style.visibility = 'hidden';
-    
- document.getElementById("procent1").style.visibility = 'hidden';
